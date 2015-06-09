0b9edfd32894cb7c0c290eb5188314a07deb8ff4
Kyle B. Johnson <kyle@wpninjas.com>
2015-06-09 00:09:21 -0400

    stub meta_box

--- mailchimp-post-to-campaign.php
+++ mailchimp-post-to-campaign.php
@@ -9,6 +9,10 @@
  * Author URI: http://kylebjohnson.me
  */
 
+if ( ! class_exists( 'Mailchimp' ) ) {
+    require_once 'Mailchimp.php';
+}
+
 class KBJ_MailChimpPostToCampaign
 {
     private $api_key = '0b69b108e80dcca17c6e53545def6836-us11';
@@ -21,19 +25,82 @@ class KBJ_MailChimpPostToCampaign
 
     public function __construct()
     {
-        add_action( 'save_post', array( $this, 'create_campaign'), 10, 3 );
+        add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
+        add_action( 'save_post', array( $this, 'save' ), 10, 3 );
     }
 
-    function create_campaign( $post_id, $post, $update )
+    public function add_meta_box()
     {
+        add_meta_box(
+            'KBJ_MailChimpPostToCampaign',
+            __( 'Post to Campaign', 'KBJ_MailChimpPostToCampaign' ),
+            array( $this, 'meta_box_callback' ),
+            'post',
+            'side'
+        );
+    }
+
+    public function meta_box_callback( $post ) {
+
+        // Add a nonce field so we can check for it later.
+        //wp_nonce_field( 'myplugin_meta_box', 'myplugin_meta_box_nonce' );
+
+        /*
+         * Use get_post_meta() to retrieve an existing value
+         * from the database and use the value for the form.
+         */
+        $value = get_post_meta( $post->ID, '_my_meta_value_key', true );
+
+        include 'views/post_meta_box.html.php';
+    }
+
+    function save( $post_id, $post, $update )
+    {
+        if( ! $this->verify_nonce( $post_id ) )
+            return $post_id;
+
+        // If this is an autosave, our form has not been submitted,
+        //      so we don't want to do anything.
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+            return $post_id;
+
+        // Check the user's permissions.
+        if ( ! current_user_can( 'edit_post', $post_id ) )
+            return $post_id;
+
+        // Sanitize the user input.
+        $mydata = sanitize_text_field( $_POST['myplugin_new_field'] );
+
+        // Update the meta field.
+        update_post_meta( $post_id, '_my_meta_value_key', $mydata );
+
+
+
+
+
         // If this is just a revision, don't send the email.
-        if ( ! $update )
-            return;
+        if ( $update && $this->settings['create_campaign'] )
+            return $post_id;
+    }
 
-        if ( ! class_exists( 'Mailchimp' ) ) {
-            require_once 'Mailchimp.php';
-        }
+    private function verify_nonce( $post_id )
+    {
+        /*
+         * We need to verify this came from the our screen and with proper authorization,
+         * because save_post can be triggered at other times.
+         */
 
+        // Check if our nonce is set.
+        if ( ! isset( $_POST['myplugin_inner_custom_box_nonce'] ) )
+            return $post_id;
+
+        $nonce = $_POST['myplugin_inner_custom_box_nonce'];
+
+        return wp_verify_nonce( $nonce, 'myplugin_inner_custom_box' );
+    }
+
+    private function create_campaign( $post )
+    {
         $opts = array(
             'ssl_verifypeer' => false,
         );
@@ -49,9 +116,9 @@ class KBJ_MailChimpPostToCampaign
             'template_id' => $this->template_id,
         );
 
-        $content['sections']['post_title'] = '<a href="' . post_permalink( $post_id ) . '">' . $post->post_title . '</a>';
+        $content['sections']['post_title'] = '<a href="' . post_permalink( $post->id ) . '">' . $post->post_title . '</a>';
         $content['sections']['post_excerpt'] = ( $post->post_excerpt ) ? $post->post_excerpt : $post->post_content ;
-        $content['sections']['after_post_excerpt'] = '<a href="' . post_permalink( $post_id ) . '">Continue reading...</a>';
+        $content['sections']['after_post_excerpt'] = '<a href="' . post_permalink( $post->id ) . '">Continue reading...</a>';
 
         try {
             $campaign = $api->campaigns->create( 'regular', $options, $content );
---
+++ views/post_meta_box.html.php
@@ -0,0 +1,11 @@
+<style>
+    .kbj__label,
+    .kbj__select {
+        display: block;
+        width: 100%;
+    }
+</style>
+<label class="kbj__label" for="">List</label>
+<select class="kbj__select" name="" id="">
+    <option value="">List One</option>
+</select>
